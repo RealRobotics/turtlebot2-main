f4a141ec4ec058e438d731f0d6627c9cc515221a
Andy Blight <a.j.blight@leeds.ac.uk>
2026-07-10 16:58:00 +0100

    Added correct contents to base only launch file

--- turtlebot2_main/launch/turtlebot2-base.launch.py
+++ turtlebot2_main/launch/turtlebot2-base.launch.py
@@ -20,20 +20,38 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+import ament_index_python.packages
 from launch import LaunchDescription
 from launch_ros.actions import Node
+import os
+import yaml
 
 
 def generate_launch_description():
 
-    base = Node(
-        package="turtlebot2_main",
-        executable="turtlebot2_main_node",
-        name="turtlebot2_main_node",
-        output="screen",
-        emulate_tty=True,
+    # Command line for the main node
+    #  ros2 launch kobuki_node kobuki_node-launch.py
+    # The stuff below is taken from the kobuki_node-launch.py file.
+
+    share_dir = ament_index_python.packages.get_package_share_directory("kobuki_node")
+
+    # There are two different ways to pass parameters to a non-composed node;
+    # either by specifying the path to the file containing the parameters, or by
+    # passing a dictionary containing the key -> value pairs of the parameters.
+    # When starting a *composed* node on the other hand, only the dictionary
+    # style is supported.  To keep the code between the non-composed and
+    # composed launch file similar, we use that style here as well.
+    params_file = os.path.join(share_dir, "config", "kobuki_node_params.yaml")
+    with open(params_file, "r") as f:
+        params = yaml.safe_load(f)["kobuki_ros_node"]["ros__parameters"]
+
+    kobuki_ros_node = Node(
+        package="kobuki_node",
+        executable="kobuki_ros_node",
+        output="both",
+        parameters=[params],
     )
 
     ld = LaunchDescription()
-    ld.add_action(base)
+    ld.add_action(kobuki_ros_node)
     return ld
